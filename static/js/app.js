f8a2e22dca2f7631216b450e599f9cdf8692bbfa
matthewjgarcia <matthewjggarcia@gmail.com>
2024-01-20 11:08:14 -1000

    first step

---
+++ static/js/app.js
@@ -0,0 +1,7 @@
+//set up the url first
+let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+//step 1.
+d3.json(url).then(function(data) {
+    console.log(data);
+});
